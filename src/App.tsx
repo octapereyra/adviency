e193f00b91fc241feae348e30dc85486beff78e8
octapereyra <octaviopereyra19@gmail.com>
2023-12-26 12:27:27 -0300

    day 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import './App.css';
 
-const regalos = ['Medias', 'Caramelos', 'Vitel Tone'];
+const regalos = () => {
+  const gifts = localStorage.getItem('gifts');
+  if (gifts) {
+    return JSON.parse(gifts) as string[];
+  } else {
+    return [];
+  }
+};
 
 function App() {
   const [gifts, setGifts] = useState(regalos);
+
+  useEffect(() => {
+    localStorage.setItem('gifts', JSON.stringify(gifts));
+  }, [gifts]);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     const fields = Object.fromEntries(
       new FormData(e.target as HTMLFormElement)
     );
-    if (fields.query === '' || gifts.includes(fields.query.toString())) return;
-    setGifts([...gifts, fields.query as string]);
+    if (fields.query === '') return;
+    if (Number.parseInt(fields.giftCount.toString()) > 1) {
+      const giftName = `${fields.query} x${fields.giftCount.toString()}`;
+      setGifts([...gifts, giftName]);
+    } else {
+      setGifts([...gifts, fields.query as string]);
+    }
   };
 
   const handleDelete = (giftSelected: string) => () => {
@@ -25,7 +41,7 @@ function App() {
 
   return (
     <div className="flex flex-row justify-center h-screen">
-      <div className="flex flex-col justify-center items-center gap-2">
+      <div className="flex flex-col justify-center items-center gap-2 w-[450px]">
         <main className=" bg-red-200 p-10 rounded-md">
           <h1 className=" font-bold text-5xl">Regalos:</h1>
           <form className="flex justify-between p-2" onSubmit={handleSubmit}>
@@ -33,6 +49,12 @@ function App() {
               type="text"
               name="query"
               className="h-9 w-2/3 border-solid border-black border rounded"
+            ></input>
+            <input
+              type="number"
+              name="giftCount"
+              min={1}
+              className="h-9 w-1/12 border-solid border-black border rounded"
             ></input>
             <button
               type="submit"
